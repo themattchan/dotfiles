7c7fc62be885c5529ae0393bf26df2c17f7e3f18
Matthew Chan <matt@themattchan.com>
2018-11-04 16:33:12 -0800

    nix

---
+++ nix/install.nix
@@ -0,0 +1,14 @@
+# nix-env --install --file install.nix
+let pkgs = import <nixpkgs> {};
+in {
+   polybar = pkgs.polybar.override {
+      i3Support = true;
+#      mpdSupport = true;
+      alsaSupport = true;
+      pulseSupport = true;
+      githubSupport = true;
+   };
+
+   inherit (pkgs)
+     rofi-unwrapped;
+}
