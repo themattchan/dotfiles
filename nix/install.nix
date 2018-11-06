1768d19ff37c597fef66ba1328f87afadb14c379
Matthew Chan <matt@themattchan.com>
2018-11-05 23:57:23 -0800

    cleanup

--- nix/install.nix
+++ nix/install.nix
@@ -5,7 +5,7 @@ with import <nixpkgs>{};
 # themes go in /share/sddm/theme
 #https://github.com/NixOS/nixpkgs/blob/release-18.09/nixos/modules/services/x11/display-managers/sddm.nix#L268
   wm = {
-    inherit (pkgs) i3 i3lock i3status;
+    inherit (pkgs) i3 i3lock i3status j4-dmenu-desktop;
 
     polybar = pkgs.polybar.override {
         i3Support = true;
